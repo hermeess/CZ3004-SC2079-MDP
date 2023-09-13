e470b153880dbc76dc17258c4c412966bf77b9d9
Jiaxi <jiangjiaxi1011@gmail.com>
2023-09-13 21:00:43 +0800

    draw bounding box

--- image-recognition/interface server/main.py
+++ image-recognition/interface server/main.py
@@ -5,8 +5,10 @@ from model import *
 app = Flask(__name__)
 CORS(app)
 
-# model = load_model()
-model = None
+model = load_model()
+# model = None
+
+UPLOAD_FOLDER = 'uploads'
 
 @app.route('/status', methods = ['GET'])
 def get_status():
@@ -17,6 +19,10 @@ def get_status():
 def image_rec():
     file = request.files['file']
     filename = file.filename
+
+    if not os.path.exists(UPLOAD_FOLDER):
+        os.makedirs(UPLOAD_FOLDER)
+
     file.save(os.path.join('uploads', filename))
 
     # TODO: rec_image function in model.py
--- image-recognition/interface server/model.py
+++ image-recognition/interface server/model.py
@@ -1,48 +1,93 @@
 from PIL import Image
+from ultralytics import YOLO
 import torch
+import os
+import cv2
+import numpy as np
 
 def load_model():
-    model = torch.hub.load('./', 'custom', path='yolo_model.pt', source='local')
+    # model = torch.hub.load('./', 'custom', path='week8.pt', source='local')
+    model = YOLO('week8.pt')
     return model
 
+def draw_bbox(img, x1, y1, x2, y2, image_id, color=(255,255,255), text_color=(0,0,0)):
+    # convert coordinates to int
+    x1 = int(x1)
+    x2 = int(x2)
+    y1 = int(y1)
+    y2 = int(y2)
+
+    id_to_name = {
+        11: 'Number 1',
+        12: 'Number 2',
+        13: 'Number 3', 
+        14: 'Number 4',
+        15: 'Number 5',
+        16: 'Number 6',
+        17: 'Number 7',
+        18: 'Number 8',
+        19: 'Number 9',
+        20: 'Alphabet A',
+        21: 'Alphabet B',
+        22: 'Alphabet C',
+        23: 'Alphabet D',
+        24: 'Alphabet E',
+        25: 'Alphabet F',
+        26: 'Alphabet G',
+        27: 'Alphabet H',
+        28: 'Alphabet S',
+        29: 'Alphabet T',
+        30: 'Alphabet U',
+        31: 'Alphabet V',
+        32: 'Alphabet W',
+        33: 'Alphabet X',
+        34: 'Alphabet Y',
+        35: 'Alphabet Z',
+        36: 'Up arrow',
+        37: 'Down arrow',
+        38: 'Right arrow',
+        39: 'Left arrow',
+        40: 'Stop',
+        99: 'Bulleye'
+    }
+
+    if not os.path.exists('image_results'):
+        os.makedirs('image_results')
+
+    # save raw image
+    img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+    cv2.imwrite(f"image_results/raw_image_{image_id}.jpg", img)
+
+    # draw bounding box
+    img = cv2.rectangle(img, (x1, y1), (x2, y2), (36,255,12), 2)
+    # print the text
+    img = cv2.rectangle(img, (x2 + 100, y1), (x2 + 450, y1 + 200), color, -1)
+    img = cv2.putText(img, id_to_name[int(image_id)], (x2 + 120, y1 + 80), cv2.FONT_HERSHEY_SIMPLEX, 1.5, text_color, 3)
+    img = cv2.putText(img, 'Image id='+image_id, (x2 + 120, y1 + 150), cv2.FONT_HERSHEY_SIMPLEX, 1.5, text_color, 3)
+    # save annotated image
+    cv2.imwrite(f"image_results/annotated_image_{image_id}.jpg", img)
+
+
 def rec_image(image, model):
-    try:
-        # load image
-        img = Image.open(os.path.join('uploads', image))
+    
+    # load image
+    img = Image.open(os.path.join('uploads', image))
+    results = model.predict(img)
+    result = results[0]
 
-        results = model(img)
+    rec_result = 'NA'
 
-        # TODO save results
+    print("-----Recognize results-----")
 
-        df_results = results.pandas().xyxy[0]
-        df_results['bboxHeight'] = df_results['ymax'] - df_results['ymin']
-        df_results['bboxWeight'] = df_results['xmax'] - df_results['xmin']
-        df_results['bboxArea'] = df_results['bboxHeight'] * df_results['bboxWeight']
+    for box in result.boxes:
+        image_id = result.names[box.cls[0].item()]
+        bbox = box.xyxy[0].tolist()
+        confidence = round(box.conf[0].item(), 3)
+        print("Image ID:", image_id)
+        print("Bounding box coordinates:", bbox)
+        print("Probability:", confidence)
+        draw_bbox(np.array(img), bbox[0], bbox[1], bbox[2], bbox[3], image_id)
+        rec_result = image_id
 
-        # sort list by bbox area
-        df_results = df_results.sort_values('bboxArea', ascending = False)
-
-        # TODO ignore bullseye need or not?
-
-        rec_result = 'NA'
-
-        if len(df_results) == 1:
-            rec_result = df_results.iloc[0]
-
-        elif len(df_results) > 1:
-            rec_shortlist = []
-            current_area = df_results.iloc[0]['bboxArea']
-            for _, row in df_results.iterrows():
-                if row['confidence'] > 0.5 and (row['bboxArea' >= current_area*0.8]):
-                    rec_shortlist.append(row)
-                    current_area = row['bboxArea']
-
-            if len(rec_shortlist) == 1:
-                rec_result = rec_shortlist[0]
-            else:
-                # TODO how to choose if multiple rec results
-                rec_result = rec_shortlist[0]
-    except:
-        print("Final result: NA")
-        return 'NA'
+    return rec_result
         
