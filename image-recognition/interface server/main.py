b6ac9ee3a279e60df91d091a4917b9ec03063ddd
Jiaxi <jiangjiaxi1011@gmail.com>
2023-10-02 19:58:14 +0800

    file name

--- image-recognition/interface server/main.py
+++ image-recognition/interface server/main.py
@@ -1,7 +1,7 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS
 from model import *
-from algorithm.mainAlgo import *
+from mainAlgo import *
 
 app = Flask(__name__)
 CORS(app)
